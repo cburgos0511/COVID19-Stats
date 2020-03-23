48ac6d1296b1bc240c0f76599c64b33df8209372
cburgos0511 <48486237+cburgos0511@users.noreply.github.com>
2020-03-23 12:56:50 -0500

    fixed the data being fetched

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,38 +4,40 @@ import TableDetail from "./TableDetails";
 
 export default function Table({ co }) {
   const { stats: detail, loading, error } = useStats(
-    `https://covid19.mathdro.id/api/countries/${co}/confirmed`
+    `https://covid19.mathdro.id/api/countries/${co}/deaths`
   );
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error..</p>;
-  const det = detail.map((item, i) => {
+  else {
+    const det = detail.map((item, i) => {
+      return (
+        <TableDetail
+          k={i}
+          state={item.provinceState}
+          active={item.active}
+          confirmed={item.confirmed}
+          death={item.death}
+          recovered={item.recovered}
+          country={item.countryRegion}
+        />
+      );
+    });
     return (
-      <TableDetail
-        k={i}
-        state={item.provinceState}
-        active={item.active}
-        confirmed={item.confirmed}
-        death={item.death}
-        recovered={item.recovered}
-        country={item.countryRegion}
-      />
+      <div className="table">
+        <table>
+          <thead>
+            <tr>
+              <th>Province/State</th>
+              <th>Confirmed</th>
+              <th>Recovered</th>
+              <th>Deaths</th>
+              <th>Active</th>
+            </tr>
+          </thead>
+          <tbody>{det}</tbody>
+        </table>
+      </div>
     );
-  });
-  return (
-    <div className="table">
-      <table>
-        <thead>
-          <tr>
-            <th>Province/State</th>
-            <th>Confirmed</th>
-            <th>Recovered</th>
-            <th>Deaths</th>
-            <th>Active</th>
-          </tr>
-        </thead>
-        <tbody>{det}</tbody>
-      </table>
-    </div>
-  );
+  }
 }
